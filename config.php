51ec83b96bcdd07d44d009c66697e96bd2da2966
Orkin <orkin2913@users.noreply.github.com>
2016-01-29 14:23:57 +0100

    Update config.php

--- config.php
+++ config.php
@@ -37,11 +37,12 @@ $config['server']['queryport'] = 10011; #Server query port
 #
 #######
 
-$config['bot']['path'] = '/home/bottest/avnbot/'; #Absolute path to bot directory
 $config['bot']['nickname'] = 'AVNBot'; #Bots name
 $config['bot']['channel'] = 2; #Bots channel
-$config['bot']['admingroups'] = '2,6'; #ID grup administracyjnych
-$config['bot']['version'] = '1.0.0'; #Wersja bota
+$config['bot']['admingroups'] = array(2, 6); #ID grup administracyjnych (po przecinku)
+$config['bot']['technician'] = array(2); #ID grup technicznych (po przecinku). Będą mogli oni zarządzać botem; będą oni również otrzymywać powiadomienia w razie problemów z botem. (wymagane permissie: i_client_private_textmessage_power == 100 oraz i_client_serverquery_view_power == 100)
+$config['bot']['speed'] = 3; #Prędkość bota (od 1 do ∞). Im mniej tym szybsze działanie bota (10 - sekunda // 1 - 1/10 sekundy). Jeżeli maszyna laguje Ci przez bota, to zwiększ tę wartość (bot może działać z opóźnieniem)
+$config['bot']['version'] = '1.1.0'; #Wersja bota
 
 #######
 # 
@@ -53,9 +54,25 @@ $config['module']['pokebot']['enable'] = true; #Pokebot
 $config['module']['nickprotect']['enable'] = true; #Ochrona nicków
 $config['module']['nickabuse']['enable'] = true; #Wyszukiwanie nadużyć w nickach
 $config['module']['channelcheck']['enable'] = true; #Sprawdzanie kanałów
-$config['module']['autoregister']['enable'] = true; #Automatyczna rejestracja
-$config['module']['channelgroups']['enable'] = true; #Ranga po wejściu na dany kanał
-$config['module']['commands']['enable'] = true; #Komendy bota
+
+
+
+#######
+# 
+# Konfiguracja komend
+#
+#######
+
+$config['module']['commands'] = array(
+											'enable' => false,
+											'datazero' => '1970-01-01 00:00:00', #Data zerowa. Przy uruchomieniu bota akcja wykona się raz bez czekania
+											'interval' => array( #Interwał sprawdzania rekordu. Co ile ma wykonywać akcję
+															'days' => 0,
+															'hours' => 0,
+															'minutes' => 0,
+															'seconds' => 1
+											)
+);
 
 
 #######
@@ -67,7 +84,7 @@ $config['module']['commands']['enable'] = true; #Komendy bota
 $config['module']['welcomemsg'] = array(
 											'enable' => true, #Włączyć? true / false
 											'mode' => 0, #0 - do wszystkich // 1 - pomiń adminów // 2 - tylko nowi użytkownicy
-											'message' => 'Witaj %CLIENT_NICKNAME%! Ostatni raz byłeś u nas %CLIENT_LASTCONNECTED%. Połączyłeś się z serwerem %CLIENT_TOTALCONNECTIONS% razy. Twoje IP: %CLIENT_IP%, Twoje unikalne ID: %CLIENT_UNIQUE_ID%' #Treść wiadomości powitalnej
+											'message' =>  file_get_contents('msg.txt') #Treść wiadomości powitalnej w pliku msg.txt. *Tego nie dotykać :)*
 );
 
 
@@ -80,9 +97,30 @@ $config['module']['welcomemsg'] = array(
 $config['module']['userecord'] = array(
 											'enable' => true, #Włączyć? true / false
 											'channel' => 5, #ID kanału na którym będzie aktualny rekord użytkowników
-											'spacer' => '[cspacer69]', #Spacer jakiego bot ma użyć w nazwie kanału
+											'name' => '[cspacer55]Rekord online: ', #Nazwa kanału, do której dopisywana będzie liczba rekordu
 											'datazero' => '1970-01-01 00:00:00', #Data zerowa. Przy uruchomieniu bota akcja wykona się raz bez czekania
 											'interval' => array( #Interwał sprawdzania rekordu. Co ile ma wykonywać akcję
+															'days' => 0,
+															'hours' => 0,
+															'minutes' => 0,
+															'seconds' => 1
+											)
+);
+
+
+
+#######
+# 
+# Konfiguracja użytkowników online
+#
+#######
+
+$config['module']['useronline'] = array(
+											'enable' => true, #Włączyć? true / false
+											'channel' => 8, #ID kanału na którym będzie aktualny status użytkowników
+											'name' => '[cspacer55]Aktualnie online: ', #Nazwa kanału, do której dopisywana będzie liczba online
+											'datazero' => '1970-01-01 00:00:00', #Data zerowa. Przy uruchomieniu bota akcja wykona się raz bez czekania
+											'interval' => array( #Interwał sprawdzania ilości userów. Co ile ma wykonywać akcję
 															'days' => 0,
 															'hours' => 0,
 															'minutes' => 0,
@@ -124,13 +162,35 @@ $config['module']['adminsonline'] = array( #Admini online
 
 $config['module']['automsg'] = array( #Automatyczna wiadomość na czacie głównym
 											'enable' => true, #Włączyć? true / false
+											'message' => file_get_contents('servermsg.txt'), #Wiadomość wysyłana na czat główny w pliku servermsg.txt
 											'datazero' => '1970-01-01 00:00:00', #Data zerowa. Przy uruchomieniu bota akcja wykona się raz bez czekania
 											'interval' => array( #Interwał wysyłania waidomości na czacie głównym. Co ile ma wykonywać akcję
 															'days' => 0,
 															'hours' => 0,
-															'minutes' => 0,
-															'seconds' => 1
+															'minutes' => 10,
+															'seconds' => 0
 											)	
+);
+
+
+
+#######
+# 
+# Konfiguracja auto rang
+#
+#######
+
+$config['module']['autogroups'] = array( #Automatyczna rejestracja / przyznawanie reng
+											'enable' => true, #Włączyć? true / false
+											'autotimeregister' => true, #Włączyć automatyczną rejestrację po czasie spędzonym na serwerze? true / false
+											'autochannelgroups' => true, #Włączyć automatyczną rejestrację po wejściu na kanał? true / false
+											'time' => 60, #Czas (w sekundach) spędzony na serwerze potrzebny do autorejestracji
+											'group' => 7, #Ranga przyznawana po czasie
+											'guest' => 8, #ID serwerowej grupy domyślnej (Guest). Jeżeli nic nie grzebałeś jako query, to domyślnie 8.
+											'channels' => array( #Lista kanałów na których przyznawane będą rangi oraz rangi, które będą na nich przyznawane
+															10 => 9, #(ID Kanału) => (ID Rangi)
+															11 => 10
+											)
 );
 
 
@@ -143,6 +203,13 @@ $config['module']['automsg'] = array( #Automatyczna wiadomość na czacie głów
 
 $config['module']['movetoafk'] = array( #Przenoszenie na kanał AFK
 											'enable' => true, #Włączyć? true / false
+											'moveadmins' => true, #Czy przenosić również adminów? true/flase
+											'kickme' => true, #Czy bot ma kickować po jakimś czasie z serwera za zajmowanie slota? true / false
+											'ignoreme' => true, #Czy dodać wyjątek do bota? Nie będzie przenoszony na kanał AFK. true / false
+											'ignore' => 11, #ID rangi którą ma ignorować bot bez względu na wszystko (np. ranga MusicBota)
+											'kicktime' => 80, #Czas nieaktywności (w sekundach) po jakim bot wyrzuci usera za AFK
+											'channel' => 9, #ID kanału AFK
+											'idletime' => 60, #Czas nieaktywności (w sekundach) po jakim bot ma przenosić
 											'datazero' => '1970-01-01 00:00:00', #Data zerowa. Przy uruchomieniu bota akcja wykona się raz bez czekania
 											'interval' => array( #Interwał przenoszenia z i do kanału AFK. Co ile ma wykonywać akcję
 															'days' => 0,
@@ -153,22 +220,28 @@ $config['module']['movetoafk'] = array( #Przenoszenie na kanał AFK
 );
 
 
-
 #######
 # 
-# Konfiguracja controlbota
+# Konfiguracja Rangi Dostępowej
 #
 #######
 
-$config['module']['controlbot'] = array( #Sprawdzanie stanu bota
+$config['module']['accessgroup'] = array( #Przenoszenie na kanał AFK
 											'enable' => true, #Włączyć? true / false
+											'blockgroup' => 9, #ID kanałowej rangi która będzie blokować wstęp. Wymagane permissie i_channel_join_power == -999
+											'guestgroup' => 8, #ID domyślnej kanałowej rangi. Zwykle 8
+											'channels' => array( #Lista kanałów na których wstęp mają tylko określone rangi
+															10 => 9, #(ID Kanału) => (ID Serwerowej rangi dostępowej)
+															11 => 10
+											),
 											'datazero' => '1970-01-01 00:00:00', #Data zerowa. Przy uruchomieniu bota akcja wykona się raz bez czekania
-											'interval' => array( #Interwał sprawdzania działania bota. Co ile ma wykonywać akcję
+											'interval' => array( #Interwał sprawdzania kto ma dostęp do kanałów. Co ile ma wykonywać akcję
 															'days' => 0,
 															'hours' => 0,
 															'minutes' => 0,
-															'seconds' => 1
-											)		
+															'seconds' => 10
+											)
 );
+
 
 ?>
